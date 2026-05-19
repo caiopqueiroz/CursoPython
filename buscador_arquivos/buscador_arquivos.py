409b5f3e47dca81ac39b1240e7127cbce50286f6
Caio Queiroz <caioq8421@gmail.com>
2026-05-18 21:00:06 -0300

    Atualizações

--- buscador_arquivos/buscador_arquivos.py
+++ buscador_arquivos/buscador_arquivos.py
@@ -1,25 +1,22 @@
-caminho = 'buscador_arquivos/teste.txt'
+caminhos = ['buscador_arquivos/teste.txt', 'buscador_arquivos/outro_teste.txt', '../VisualizaçãoDados/Linguagem R.txt']
+encontrou = False
 print('Buscador de arquivos')
 comando = str(input('Digite a palavra que deseja buscar: '))
-resultado = 'Não encontrei'
-arquivo = open(caminho, 'rt')
-# para cada linha no arquivo
-for linha in arquivo:
-    # criando uma lista de palavras presentes na linha
-    palavras = linha.split(' ')
-    # para cada palvra nessa lista de palavras
-    for palavra in palavras:
-        # se o comando for a palavra 
-        if comando in palavra:
-            resultado = 'Encontrei'
-print(resultado)
-
+for elemento in caminhos:
+    caminho = elemento
+    arquivo = open(caminho, 'rt')
+    titulo = caminho.split('/')
     
+    for elemento in titulo:
+        if comando in elemento:
+            print(f'\nExiste um arquivo com esse nome - \033[34m{caminho}\033[m')
 
-
-
-
-
-
-# caneta = open(arquivo, 'at')
-#caneta.write(palavra)
+    for linha in arquivo:
+        palavras = linha.split(' ')
+        for palavra in palavras:
+            if comando in palavra:
+                encontrou = True
+                local = caminho
+                print(f'\nEncontrei essa palavra no arquivo \033[32m{caminho}\033[m')
+if not encontrou:
+    print('\n\033[31mNão encontrei essa palavra em nenhum arquivo\033[m')
---
+++ buscador_arquivos/teste.py
@@ -0,0 +1,3 @@
+palavra = 'buscador_arquivos/arquivo.txt'
+palavras = palavra.split('/')
+print(palavras)
