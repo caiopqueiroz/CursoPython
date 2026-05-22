43beb73830ad6e17800608d6b4b1fb1173166716
Caio Queiroz <caioq8421@gmail.com>
2026-05-22 15:11:40 -0300

    Atualizações

--- buscador_arquivos/buscador_arquivos.py
+++ buscador_arquivos/buscador_arquivos.py
@@ -1,6 +1,8 @@
 caminhos = ['buscador_arquivos/teste.txt', 'buscador_arquivos/outro_teste.txt', '../VisualizaçãoDados/Linguagem R.txt', '../../Caio/Ideias/Buscador de arquivos.txt']
 encontrou = False
 escrever_frase = False
+fim_linha = True
+contador = 0
 print('Buscador de arquivos\n')
 comando = str(input('Digite a palavra que deseja buscar: '))
 for elemento in caminhos:
@@ -15,11 +17,11 @@ for elemento in caminhos:
 for elemento in caminhos:
     caminho = elemento
     arquivo = open(caminho, 'rt', encoding='utf-8')
-    titulo = caminho.split('/')
     
     for linha in arquivo:
         palavras = linha.split(' ')
-        
+        contador_palavras = 0
+
         for palavra in palavras:
             if palavra == '\n':
                 palavras.remove(palavra)
@@ -31,25 +33,31 @@ for elemento in caminhos:
                     palavras.append(palavra_corrigida[0])        
         
         for palavra in palavras:
+            contador_palavras += 1
+
             if comando in palavra:
                 frase = list()
                 contador = 0
                 escrever_frase = True
                 encontrou = True
+                fim_linha = False
                 local = caminho
                 print(f'\nEncontrei essa palavra no arquivo \033[32m{caminho}\033[m')
             
-            if escrever_frase:
-                if contador <= 5:
-                    frase.append(palavra)
-                    contador += 1
-                else:
-                    # Resolver o problema com a quebra de linha no meio da frase
-                    print(frase)
+            if not fim_linha:
+                if escrever_frase:
+                    if contador <= 5:
+                        frase.append(palavra)
+                        contador += 1
+
+            if contador_palavras == len(palavras) or contador > 5:
+                fim_linha = True
+                if escrever_frase:
                     print('Prévia: ', end='')
                     for elemento in frase:
                         print(f'\033[7;2m{elemento} \033[m', end='')
                     print()
                     escrever_frase = False
+
 if not encontrou:
     print('\n\033[31mNão encontrei essa palavra em nenhum arquivo\033[m')
